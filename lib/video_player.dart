f006c73fb5e4b53b2184d38c1c17a6d9ffcf76df
tomaristark <107350576+tomaristark@users.noreply.github.com>
2024-02-18 21:05:28 +0630

    add-video-player

---
+++ lib/video_player.dart
@@ -0,0 +1,98 @@
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerView extends StatefulWidget {
+  const VideoPlayerView({
+    super.key,
+    required this.url,
+    required this.dataSourceType,
+  });
+
+  final String url;
+
+  final DataSourceType dataSourceType;
+
+  @override
+  State<VideoPlayerView> createState() => _VideoPlayerViewState();
+}
+
+class _VideoPlayerViewState extends State<VideoPlayerView> {
+  late VideoPlayerController _videoPlayerController;
+
+   ChewieController ? _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    switch (widget.dataSourceType) {
+      case DataSourceType.asset:
+        _videoPlayerController = VideoPlayerController.asset(widget.url);
+        break;
+      case DataSourceType.network:
+        _videoPlayerController = VideoPlayerController.networkUrl(Uri.parse(widget.url));
+        break;
+      case DataSourceType.file:
+        
+        break;
+      case DataSourceType.contentUri:
+        _videoPlayerController =
+            VideoPlayerController.contentUri(Uri.parse(widget.url));
+        break;
+    }
+
+    // _videoPlayerController.initialize().then(
+    //       (_) => setState(
+    //         () =>
+    //         ),
+    //       ),
+    //     );
+    initializeVideo();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+Future initializeVideo ()async {
+  await _videoPlayerController.initialize();
+
+
+   _chewieController = ChewieController(
+              videoPlayerController: _videoPlayerController,
+              aspectRatio: 16 / 9,);
+  
+  setState(() {
+    
+  });
+}
+  @override
+  Widget build(BuildContext context) {
+    if(_chewieController  == null){
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    } 
+    else{
+       return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.dataSourceType.name.toUpperCase(),
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        const Divider(),
+        AspectRatio(
+          aspectRatio: 16 / 9,
+          child: Chewie(controller: _chewieController!),
+        ),
+      ],
+    );
+    }
+
+  }
+}
